775f0c28a70ccde07dcea4b4781ab9fa92e53184
visioncomputer200@gmail.com <visioncomputer200@gmail.com>
2025-11-11 17:11:13 +0000

    Changed Graph Interface

--- include/graph.hpp
+++ include/graph.hpp
@@ -3,6 +3,7 @@
 #define GRAPHX_INCLUDE_GRAPH_H_
 
 #include <vector>
+#include <optional>
 #include <stdexcept>
 
 inline constexpr int kDefaultWeight = 1;
@@ -15,29 +16,42 @@ inline constexpr int kDefaultWeight = 1;
 #endif
 
 namespace graphx {
-	template <typename NodeType>
+	template <typename VertexType, typename WeightType = int>
     class Graph {
         public: 
             virtual ~Graph() = default;
 			
-			virtual void add_node(const NodeType& node) = 0;
-			virtual bool remove_node(const NodeType& node) = 0;
-            virtual void add_edge(const NodeType& node1, const NodeType& node2, int weight = DEFAULT_WEIGHT) = 0;
-            virtual bool remove_edge(const NodeType& node1, const NodeType& node2) = 0;
+			// Core operations
+			virtual void add_vertex(const VertexType& vertex) = 0;
+			virtual bool remove_vertex(const VertexType& vertex) = 0;
+            virtual void add_edge(const VertexType& u, const VertexType& v, WeightType weight = DEFAULT_WEIGHT) = 0;
+            virtual bool remove_edge(const VertexType& u, const VertexType& v) = 0;
             
-            virtual bool has_node(const NodeType& node) = 0;
-            virtual bool has_edge(const NodeType& node1, const NodeType& node2) const = 0;
-
-            virtual std::vector<NodeType> get_neighbors(const NodeType& node) const = 0;
-            virtual int get_edge_weight(const NodeType& node1, const NodeType& node2) const = 0;
-            virtual std::vector<std::vector<int>> get_matrix() const = 0;
-            virtual int get_num_vertices() const = 0;
-            virtual void print_matrix() const = 0;
+            // Queries
+            virtual bool has_vertex(const VertexType& vertex) const = 0;
+            virtual bool has_edge(const VertexType& u, const VertexType& v) const = 0;
+			
+			// Accessors
+            virtual std::vector<VertexType> neighbors(const VertexType& vertex) const = 0;
+            virtual std::vector<VertexType> vertices() const = 0;
+            virtual std::vector<std::pair<VertexType, VertexType>> edges() const = 0;
+            virtual std::optional<WeightType> get_edge_weight(const VertexType& u, const VertexType& v) const = 0;
+            virtual int degree(const VertexType& vertex) const = 0;
+			virtual int in_degree(const VertexType& vertex) const = 0;
+			virtual int out_degree(const VertexType& vertex) const = 0;
+			
+            // Stats
+            virtual int vertex_count() const = 0;
+            virtual int edge_count() const = 0;
+            virtual bool is_directed() const = 0;
+			virtual bool is_weighted() const = 0;
+			
+			// Utilities
             virtual void multiply_by_minus_one() = 0;
-
-            virtual std::vector<std::pair<int, int>> get_edges() const = 0;
-            virtual int get_node_count() const = 0;
-            virtual int get_edge_count() const = 0;
+            virtual void clear() = 0;
+			virtual void print(std::ostream& os = std::cout) const = 0;
+			virtual void save_to_file(const std::string& filename) const = 0;
+			virtual void load_from_file(const std::string& filename) = 0;
     };
 }
 #endif // GRAPHX_INCLUDE_GRAPH_H_
